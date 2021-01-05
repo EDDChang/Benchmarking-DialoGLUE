f4151b56d84f3f89a936c8398c2793774aec8035
b06902073 <b06902073@meow2.csie.ntu.edu.tw>
2021-01-05 22:09:44 +0800

    py new

--- bert_models_intent.py
+++ bert_models_intent.py
@@ -1,3 +1,4 @@
+import sys
 import torch
 import torch.nn.functional as F
 
@@ -34,39 +35,57 @@ class IntentBertModel(torch.nn.Module):
         self.num_intent_labels = num_intent_labels
         self.intent_classifier = nn.Linear(self.bert_model.config.hidden_size, num_intent_labels)
         self.labels_info = labels_info
+        self.alpha = torch.tensor(0.2)
     def forward(self,
                 input_ids: torch.tensor,
                 attention_mask: torch.tensor,
                 token_type_ids: torch.tensor,
                 intent_label: torch.tensor = None,
                 device='cuda'):
-        print(self.labels_info)
+
         pooled_output = self.bert_model(input_ids=input_ids,
                                         attention_mask=attention_mask,
                                         token_type_ids=token_type_ids)[1]
-       
-        labels_embedding_list = self.bert_model(input_ids=self.labels_info['input_ids'].to(device),
-                                           attention_mask=self.labels_info['attention_mask'].to(device),
-                                           token_type_ids=self.labels_info['token_type_ids'].to(device))[1]
-        lr = 0.2
-        dp = self.dropout(pooled_output)
+
+        dp = self.dropout(pooled_output) 
+        intent_logits = self.intent_classifier(dp)
+        eudist = torch.tensor(0.0).to(device)
         cossim_sum = torch.tensor(0.0).to(device)
-        cos = nn.CosineSimilarity(dim=0, eps=1e-6)
-        if intent_label is not None:   
+        if intent_label is not None:
+            cos = nn.CosineSimilarity(dim = 0, eps=1e-6)
+            labels_embedding_list = self.bert_model(input_ids=self.labels_info['input_ids'].to(device),
+                                            attention_mask=self.labels_info['attention_mask'].to(device),
+                                            token_type_ids=self.labels_info['token_type_ids'].to(device))[1]
+        
+            labels_embedding_list = self.dropout(labels_embedding_list) 
+        
+            #target_embedding = []
+            #for label in intent_label:
+            #    target_embedding.append(labels_embedding_list[label])
+            
+            #target_embedding = torch.stack(target_embedding)
+            #eudist = torch.pow(dp - target_embedding, 2).sum(1).mean()
+            #eudist = eudist
             for i in range(len(input_ids)):
                 index = intent_label[i]
                 cossim_sum += cos(labels_embedding_list[index], dp[i])
-        cossim_sum /= 16
-        intent_logits = self.intent_classifier(dp)
-
-        # Compute losses if labels provided
+                #eudist += (torch.pow(labels_embedding_list[index] - dp[i], 2).sum()/len(input_ids))
+            #eudist /= 25
+            cossim_sum /= len(input_ids)
+            labels_logits = self.intent_classifier(labels_embedding_list)
+        else:
+            eudist = torch.tensor(0.0)
+            
         if intent_label is not None:
             loss_fct = CrossEntropyLoss()
             intent_loss = loss_fct(intent_logits.view(-1, self.num_intent_labels), intent_label.type(torch.long))
+            labels_loss = loss_fct(labels_logits.view(-1, self.num_intent_labels), self.labels_info['labels'].to(device).type(torch.long))
+            print(intent_loss, cossim_sum, labels_loss)
         else:
-            intent_loss = torch.tensor(0)
-        intent_loss -= lr * cossim_sum
-        return intent_logits, intent_loss
+            labels_loss = torch.tensor(0.0)
+            intent_loss = torch.tensor(0.0)
+        
+        return intent_logits , (1-self.alpha)*intent_loss + self.alpha*torch.log(1/cossim_sum)
 
 class SlotBertModel(torch.nn.Module):
     def __init__(self,
